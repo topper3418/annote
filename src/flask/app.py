1233b6fd076ed533e04e8425d7126d012203c3f9
topper3418 <67675579+topper3418@users.noreply.github.com>
2024-08-24 23:32:50 -0500

    commented the flask bit

--- src/flask/app.py
+++ src/flask/app.py
@@ -5,5 +5,13 @@
 #   allows to see parent
 #   allows to see unfocused siblings
 
+# thats the old one above
+# GET / -> all top level tasks, as objects populated with minimal stuff
+# GET /:taskid -> gets the details for a task, can be big if a top level task
+# GET /entries -> gets the entries in current focus scope
+# GET /entries/:entryid -> gets the metadata for the entry
+# POST / -> create an entry
+# POST /command -> post specifically a command
+
 
 # post / allows you to create entries
